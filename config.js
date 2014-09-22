66afb974a20b9d1529bca05752652dbfc54a5c1a
morpheyesh <morpheyesh@gmail.com>
2014-09-23 01:34:46 +0530

    done

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'enter-url-here',
+        url: 'http://debugall.azurewebsites.net',
         mail: {},
         database: {
             client: 'sqlite3',
